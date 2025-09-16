afc0452292d5a0709adbe2e4e3d29803cbdaae66
Anton Smedman <anton.smedman@chasacademy.se>
2025-09-16 15:52:32 +0200

    POCO Transactions

---
+++ BankApp2/Transactions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp2
+{
+    internal class Transactions
+    {
+        public string sendMoney { get; set; }
+
+        public int getMoney { get; set; }
+    }
+}
